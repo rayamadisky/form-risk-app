b10d11184a6f3c8c5b8c7ba223548c434a776d16
rayamadisky <amadisky@gmail.com>
2026-04-20 10:02:54 +0700

    Fix mixed content HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Maksa Laravel pakai HTTPS kalau bukan di XAMPP lokal
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
